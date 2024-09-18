6165c65ea452d45b4143421cec2ac90c3bb53631
Danil Kokh <shadxst@gmail.com>
2024-09-18 18:16:01 +0700

    Add migrations

--- database/migrations/2024_09_18_091109_create_payment_rules_table.php
+++ database/migrations/2024_09_18_091109_create_payment_rules_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('payment_rules', function (Blueprint $table) {
             $table->id();
+            $table->string('type');
             $table->timestamps();
         });
     }
